e3178490186fc5a91728d2cdbd7e351fd746dc99
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-03 14:04:22 +0100

    Create 1_navegacion.py

---
+++ pages/1_navegacion.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(page_title="Herramienta de navegación")
+
+col1, col2, col3 = st.columns([3,3,3])
+with col1:
+    st.image("amtega_logo.png_2089811488.png", use_column_width=True)
+with col3:
+    st.header("SMETRIA")
+
+st.divider()
+
+st.header("Herramienta de navegacion geospacial")
+# Aquí incluirías todo el código específico para la predicción
