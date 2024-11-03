f95743a251c72150453fbbc21a01cb77b0cc4d41
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-03 19:12:12 +0100

    Update 2_Herramienta_de_navegacion.py

--- pages/2_Herramienta_de_navegacion.py
+++ pages/2_Herramienta_de_navegacion.py
@@ -15,30 +15,21 @@ st.divider()
 st.header("Herramienta de navegacion geospacial")
 
 
-# Obtener la fecha actual y calcular el próximo año
 today = datetime.datetime.now()
 next_year = today.year + 1
 jan_1 = datetime.date(next_year, 1, 1)
 dec_31 = datetime.date(next_year, 12, 31)
 
-# Definir un valor predeterminado para las fechas
-default_end_date = datetime.date(next_year, 1, 7)
-
-# Usar date_input con una tupla para el rango de fechas
 d = st.date_input(
     "Select your vacation for next year",
-    (jan_1, default_end_date),
+    (jan_1, datetime.date(next_year, 1, 7)),
     jan_1,
     dec_31,
     format="MM.DD.YYYY",
 )
 
-# Comprobar si se seleccionó un rango de fechas
-if isinstance(d, tuple):
-    start_date, end_date = d
-else:
-    start_date = d
-    end_date = start_date  # Si solo se selecciona una fecha, usarla como fin
+# Desestructurar las fechas de inicio y fin
+start_date, end_date = d
 
 # Mostrar las fechas
 st.write("Start Date:", start_date)
