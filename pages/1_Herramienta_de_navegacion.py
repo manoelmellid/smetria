e3abc0ea9a6fd3833645a7135086f0555406c6db
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-03 14:05:07 +0100

    Update 1_Herramienta_de_navegacion.py

--- pages/1_Herramienta_de_navegacion.py
+++ pages/1_Herramienta_de_navegacion.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_page_config(page_title="Herramienta de navegación")
-
 col1, col2, col3 = st.columns([3,3,3])
 with col1:
     st.image("amtega_logo.png_2089811488.png", use_column_width=True)
