fd00e13930844c233d29ddcb174aa3f8008bd5d5
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-03 20:18:36 +0100

    Update 2_Herramienta_de_navegacion.py

--- pages/2_Herramienta_de_navegacion.py
+++ pages/2_Herramienta_de_navegacion.py
@@ -13,23 +13,3 @@ with col3:
 
 st.divider()
 st.header("Herramienta de navegacion geospacial")
-
-today = datetime.datetime.now()
-
-# Selección de fechas
-d = st.date_input(
-    "Select your vacation for next year",
-    (today, today + datetime.timedelta(days=3)),  # Rango de hoy a 3 días después
-    today,
-    today + datetime.timedelta(days=3),  # Fecha máxima
-    format="MM.DD.YYYY",
-)
-
-# Comprobar si se seleccionaron dos fechas
-if isinstance(d, tuple) and len(d) == 2:
-    start_date, end_date = d
-    # Mostrar las fechas
-    st.write("Start Date:", start_date)
-    st.write("End Date:", end_date)
-else:
-    st.error("Por favor selecciona una fecha de inicio y de fin")
