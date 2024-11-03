6d43f985ee96a2ce6d3fc99e4255546ad998c951
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-03 17:44:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 st.cache_data.clear()
 
-st.write("Versión de Streamlit:", st.__version__)
-
 col1, col2, col3 = st.columns([3,3,3])
 with col1:
     st.image("amtega_logo.png_2089811488.png", use_column_width=True)
