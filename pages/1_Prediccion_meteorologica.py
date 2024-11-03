1472ee5abd1c65035eed8fae9c031aeec66dfd5b
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-03 18:50:10 +0100

    Update 1_Prediccion_meteorologica.py

--- pages/1_Prediccion_meteorologica.py
+++ pages/1_Prediccion_meteorologica.py
@@ -70,8 +70,7 @@ with st.form(key='my_form'):
             
             # Imprimir las coordenadas
             if longitud is not None and latitud is not None:
-                st.write("Coordenadas:")
-                st.write(f"{latitud}, {longitud}")
+                st.write("Coordenadas:", latitud,",",longitud)
             else:
                 st.write("No se encontraron resultados para el valor de Km proporcionado.")
         else:
