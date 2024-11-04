97899f1265278ddba083c52adaf696c00d1fd07b
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-04 15:57:36 +0100

    Update resumen_datos.py

--- utils/resumen_datos.py
+++ utils/resumen_datos.py
@@ -1,5 +1,6 @@
 import csv
 import streamlit as st
+import pandas as pd
 
 def temperaturas(archivo_csv):
     temperaturas = []
@@ -17,3 +18,29 @@ def temperaturas(archivo_csv):
 
     st.write(f"Temperaura máxima: {maximo}")
     st.write(f"Temperaura minima: {minimo}")
+
+def analizar_temperaturas(archivo_csv):
+    # Cargar el archivo CSV en un DataFrame
+    df = pd.read_csv(archivo_csv)
+
+    # Convertir la columna 'temperature' a numérico
+    df['temperature'] = pd.to_numeric(df['temperature'], errors='coerce')
+
+    # Filtrar valores NaN
+    df = df.dropna(subset=['temperature'])
+
+    # Obtener la lista de temperaturas
+    temperaturas = df['temperature'].tolist()
+
+    # Calcular el máximo y el mínimo
+    maximo = max(temperaturas) if temperaturas else None
+    minimo = min(temperaturas) if temperaturas else None
+
+    col1, col2 = st.columns([2,2])
+    # Mostrar en la interfaz de Streamlit
+    with col1:
+        st.metric(label="Temperatura Máxima", value=maximo, delta="1.2 °F")
+    with col2:
+        st.metric(label="Temperatura Mínima", value=minimo, delta="0.0 °F")
+
+# Aquí deberías invocar la función, proporcionando el nombre del archivo CSV que deseas analizar
