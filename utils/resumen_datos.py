f0f0509c32e3e466443b4b5f0b8f7a55fd496658
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-04 10:49:14 +0100

    Update resumen_datos.py

--- utils/resumen_datos.py
+++ utils/resumen_datos.py
@@ -14,9 +14,5 @@ def temperaturas(archivo_csv):
     media = sum(temperaturas) / len(temperaturas) if temperaturas else 0
     maximo = max(temperaturas) if temperaturas else None
     minimo = min(temperaturas) if temperaturas else None
-
-    print("La temperatura media es: ", media)
-    print("La temperatura máxima es: ", maximo)
-    print("La temperatura mínima es: ", minimo)
     
     return media, maximo, minimo
