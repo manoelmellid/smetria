1fb94f57214a1475e15cd4d36eb36dd83a64ab17
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-05 19:33:27 +0100

    Update 5_Notificacion_de_indidencias.py

--- pages/5_Notificacion_de_indidencias.py
+++ pages/5_Notificacion_de_indidencias.py
@@ -17,6 +17,21 @@ def guardar_en_archivo(nombre, email, mensaje):
         writer = csv.writer(file)
         writer.writerow([nombre, email, mensaje])
 
+def visualizar_archivo():
+    # Leer el archivo contactos.csv y cargarlo en una lista
+    try:
+        with open('contactos.csv', mode='r') as file:
+            reader = csv.reader(file)
+            datos = list(reader)
+            if datos:
+                # Crear un dataframe a partir de los datos y mostrarlo
+                st.write("### Incidencias Registradas")
+                st.dataframe(datos)  # Puedes usar st.table(datos) si no quieres la funcionalidad interactiva
+            else:
+                st.write("No hay datos disponibles.")
+    except FileNotFoundError:
+        st.write("El archivo contactos.csv no existe aún.")
+
 col1, col2, col3 = st.columns([2,2,2])
 with col2:
     st.session_state.logged_in = gen.login()
@@ -24,7 +39,7 @@ with col2:
 # Si está logueado, muestra las vistas según el rol
 if st.session_state.logged_in == True:
     st.write("Bienvenido al área de personal.")
-    st.write("Datos privados de flujo...")
+    visualizar_archivo()
 else:
     # Campos del formulario
     nombre = st.text_input("Nombre")
