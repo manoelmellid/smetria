634e78a82d581ea56c4c4bf87f59d33410d0098e
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-05 18:50:05 +0100

    Update 3_Modelo_predictivo_de_flujos.py

--- pages/3_Modelo_predictivo_de_flujos.py
+++ pages/3_Modelo_predictivo_de_flujos.py
@@ -14,35 +14,36 @@ st.divider()
 
 st.header("Modelo predictivo de flujos")
 # ---------------------------------------------------------------------------------
-
-# Crear un sistema simple de login manual
 def login():
     if 'logged_in' not in st.session_state:
         st.session_state.logged_in = False
+        st.session_state.role = None
     
+    # Si no está logueado, el sistema solo permitirá el login si el usuario es admin
     if not st.session_state.logged_in:
-        st.subheader("Inicio de sesión")
         username = st.text_input("Usuario")
         password = st.text_input("Contraseña", type="password")
         
         if st.button("Iniciar sesión"):
-            if username == 'usuario' and password == 'password123':
-                st.session_state.logged_in = True
-                st.session_state.role = 'user'
-                st.success("Bienvenido, Usuario Normal")
-            elif username == 'admin' and password == 'adminpassword':
+            # Solo se permite login para el admin
+            if username == 'admin' and password == 'adminpassword':
                 st.session_state.logged_in = True
                 st.session_state.role = 'admin'
-                st.success("Bienvenido, Personal")
+                st.success("Bienvenido, Administrador")
             else:
-                st.error("Usuario o contraseña incorrectos")
+                st.session_state.role = 'user'
+                st.success("Bienvenido, Usuario Normal")
+                st.session_state.logged_in = True
     
-    else:
+    # Si está logueado, muestra las vistas según el rol
+    if st.session_state.logged_in:
         if st.session_state.role == 'admin':
             st.write("Bienvenido al área de personal.")
             st.write("Datos privados de flujo...")
-        else:
+        elif st.session_state.role == 'user':
             st.write("Bienvenido, usuario normal.")
             st.write("Información pública...")
+        else:
+            st.error("Rol no reconocido")
 
 login()
