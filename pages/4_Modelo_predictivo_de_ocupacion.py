8daf5d1574ee439ea8e9c7acbfe44035b85324c7
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-05 18:46:57 +0100

    Update 4_Modelo_predictivo_de_ocupacion.py

--- pages/4_Modelo_predictivo_de_ocupacion.py
+++ pages/4_Modelo_predictivo_de_ocupacion.py
@@ -16,35 +16,3 @@ st.header("Modelo predictivo de ocupacion")
 
 # ---------------------------------------------------------------------
 
-# Crear un sistema simple de login manual
-def login():
-    if 'logged_in' not in st.session_state:
-        st.session_state.logged_in = False
-    
-    if not st.session_state.logged_in:
-        st.subheader("Inicio de sesión")
-        username = st.text_input("Usuario")
-        password = st.text_input("Contraseña", type="password")
-        
-        if st.button("Iniciar sesión"):
-            if username == 'usuario' and password == 'password123':
-                st.session_state.logged_in = True
-                st.session_state.role = 'user'
-                st.success("Bienvenido, Usuario Normal")
-            elif username == 'admin' and password == 'adminpassword':
-                st.session_state.logged_in = True
-                st.session_state.role = 'admin'
-                st.success("Bienvenido, Personal")
-            else:
-                st.error("Usuario o contraseña incorrectos")
-    
-    else:
-        if st.session_state.role == 'admin':
-            st.write("Bienvenido al área de personal.")
-            st.write("Datos privados de flujo...")
-        else:
-            st.write("Bienvenido, usuario normal.")
-            st.write("Información pública...")
-
-login()
-
