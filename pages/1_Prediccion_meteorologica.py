87d0691560630b20db3a3cb2d5179abd245737fb
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-05 21:34:47 +0100

    Update 1_Prediccion_meteorologica.py

--- pages/1_Prediccion_meteorologica.py
+++ pages/1_Prediccion_meteorologica.py
@@ -99,9 +99,11 @@ if latitud is not None and longitud is not None:
         'lon': [longitud]
     }, index=[0])  # Proporciona un índice
 
-    # Mostrar el mapa solo si los datos son válidos
-    gen.mostrar_puntos_con_arcos(latitud, longitud, altura_columna=500)
-    st.map(data)
+    col1, col2 = st.columns([2,2])
+    with col1:
+        gen.mostrar_puntos_con_arcos(latitud, longitud, altura_columna=500)
+    with col2:
+        st.map(data) # Mostrar el mapa solo si los datos son válidos
 
 if adelante is not None and longitud is not None:
     # Pronostico
