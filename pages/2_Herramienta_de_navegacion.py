262f2927dd038a0e2970d41cef702fb92fcd398a
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-05 10:16:48 +0100

    Update 2_Herramienta_de_navegacion.py

--- pages/2_Herramienta_de_navegacion.py
+++ pages/2_Herramienta_de_navegacion.py
@@ -137,7 +137,7 @@ if submit_button:
                     ))
 
                     # Mostrar tabla con detalles
-                    st.write(df_filtrado[['enderezo', 'tipo', 'nome', 'distancia_km']].sort_values(by='distancia_km'))
+                    st.write(df_filtrado[['enderezo', 'concello', 'tipo', 'nome', 'distancia_km']].sort_values(by='distancia_km'))
         except ValueError:
             st.error("Por favor, ingresa un número válido.")
     else:
