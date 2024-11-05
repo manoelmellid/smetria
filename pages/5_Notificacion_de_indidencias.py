cc159305ffd90107d994e544173c760cf2028b2b
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-05 19:22:22 +0100

    Update 5_Notificacion_de_indidencias.py

--- pages/5_Notificacion_de_indidencias.py
+++ pages/5_Notificacion_de_indidencias.py
@@ -22,25 +22,21 @@ if st.session_state.logged_in == True:
     st.write("Bienvenido al área de personal.")
     st.write("Datos privados de flujo...")
 else:
-    st.write("Bienvenido, usuario normal.")
-    st.write("Información pública...")
-
-# Campos del formulario
-nombre = st.text_input("Nombre")
-email = st.text_input("Correo electrónico")
-mensaje = st.text_area("Mensaje")
+    # Campos del formulario
+    nombre = st.text_input("Nombre")
+    email = st.text_input("Correo electrónico")
+    mensaje = st.text_area("Mensaje")
+    # Usar la función cuando el formulario se envíe
+    if st.button("Enviar"):
+        if nombre and email and mensaje:
+            guardar_en_archivo(nombre, email, mensaje)
+            st.success("¡Mensaje guardado con éxito!")
+        else:
+            st.error("Por favor, llena todos los campos.")
 
 def guardar_en_archivo(nombre, email, mensaje):
     with open('contactos.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([nombre, email, mensaje])
-
-# Usar la función cuando el formulario se envíe
-if st.button("Enviar"):
-    if nombre and email and mensaje:
-        guardar_en_archivo(nombre, email, mensaje)
-        st.success("¡Mensaje guardado con éxito!")
-    else:
-        st.error("Por favor, llena todos los campos.")
 
 
