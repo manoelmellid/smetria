fa4dee2565917eb7b3e42fa7fd8235b11b4b0041
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-06 10:04:08 +0100

    Update page_recepcion.py

--- pages/page_recepcion.py
+++ pages/page_recepcion.py
@@ -3,4 +3,10 @@ from utils import general as gen
 
 st.header("Modelo predictivo de flujos")
 # ---------------------------------------------------------------------------------
-gen.login()
+
+st.session_state.logged_in = gen.login()
+# Si está logueado, muestra las vistas según el rol
+if st.session_state.logged_in == True:
+    st.write("Bienvenido al área de personal.")
+    visualizar_archivo()
+else:
