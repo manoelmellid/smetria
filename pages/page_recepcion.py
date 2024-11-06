21c922a83f8dee12a91deb5ff7b0de2e6219d1e8
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-06 10:02:28 +0100

    Create page_recepcion.py

---
+++ pages/page_recepcion.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from utils import general as gen
+
+st.header("Modelo predictivo de flujos")
+# ---------------------------------------------------------------------------------
+gen.login()
