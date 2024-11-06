4e29d5727b2652cf1c72ce09cd2b60832b617891
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-06 09:57:25 +0100

    Update page_navegacion.py

--- pages/page_navegacion.py
+++ pages/page_navegacion.py
@@ -10,17 +10,6 @@ from utils import consultas_camino as concam
 longitud = None
 latitud = None
 
-st.set_page_config(page_title="Herramienta de Navegación")
-
-# Configurar las columnas para el diseño
-col1, col2, col3 = st.columns([3, 3, 3])
-with col1:
-    st.image("amtega_logo.png_2089811488.png", use_column_width=True)
-with col3:
-    st.header("SMETRIA")
-
-st.divider()
-
 st.header("Herramientas de navegación")
 st.header("Parámetros de Filtro")
 
