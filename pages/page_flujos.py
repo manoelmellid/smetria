f28a32152db6ad41b699a083bf88c845341ab83d
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-06 09:58:44 +0100

    Update page_flujos.py

--- pages/page_flujos.py
+++ pages/page_flujos.py
@@ -1,16 +1,6 @@
 import streamlit as st
 from utils import general as gen
 
-st.set_page_config(page_title="Modelo predictivo de flujos")
-
-col1, col2, col3 = st.columns([3,3,3])
-with col1:
-    st.image("amtega_logo.png_2089811488.png", use_column_width=True)
-with col3:
-    st.header("SMETRIA")
-
-st.divider()
-
 st.header("Modelo predictivo de flujos")
 # ---------------------------------------------------------------------------------
 gen.login()
