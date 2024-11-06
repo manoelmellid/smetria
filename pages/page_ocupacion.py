73496ae0cfb85606135a3fca6714c0cc068a1949
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-06 09:57:05 +0100

    Update page_ocupacion.py

--- pages/page_ocupacion.py
+++ pages/page_ocupacion.py
@@ -2,16 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_page_config(page_title="Modelo predictivo de ocupación")
-
-col1, col2, col3 = st.columns([3,3,3])
-with col1:
-    st.image("amtega_logo.png_2089811488.png", use_column_width=True)
-with col3:
-    st.header("SMETRIA")
-
-st.divider()
-
 st.header("Modelo predictivo de ocupacion")
 
 # ---------------------------------------------------------------------
