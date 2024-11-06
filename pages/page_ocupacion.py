c4cb8d4396458632e17b8d6f9e1860a9df3e76c6
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-06 10:25:53 +0100

    Update page_ocupacion.py

--- pages/page_ocupacion.py
+++ pages/page_ocupacion.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from utils import github as git
 
 st.header("Modelo predictivo de ocupacion")
 
 # ---------------------------------------------------------------------
 
+# Ejemplo de uso
+nombre = 'Juan Pérez'
+email = 'juan.perez@email.com'
+mensaje = 'Este es un mensaje de prueba.'
+git.guardar_en_archivo(nombre, email, mensaje)
