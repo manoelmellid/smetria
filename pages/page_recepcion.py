93a46ccf49bef645b6a63d8bb96dbe892a378bed
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-06 11:11:00 +0100

    Update page_recepcion.py

--- pages/page_recepcion.py
+++ pages/page_recepcion.py
@@ -8,7 +8,7 @@ st.session_state.logged_in = gen.login()
 # Si está logueado, muestra las vistas según el rol
 if st.session_state.logged_in == True:
     st.write("Bienvenido al área de personal.")
-    df = git.leer_archivo_github()
+    df = git.visualizar_archivo()
     if not df.empty:
         st.write(df)
     else:
