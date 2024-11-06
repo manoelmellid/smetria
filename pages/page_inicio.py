1d65fc1dc926feee284a92441eb061d7d9e5eaa3
caroolrey <145906021+caroolrey@users.noreply.github.com>
2024-11-06 15:42:06 +0100

    Update page_inicio.py

--- pages/page_inicio.py
+++ pages/page_inicio.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-st.write("""SMETRIA cuenta con 4 funcionalidades para el usuario, un sistema de predicción meteorológica, una herramienta de consultas
-geospaciales, un modelo predictivo de flujos y un modelo predictivo de ocupación
+st.write("""SMETRIA cuenta con 4 funcionalidades para el usuario: un sistema de predicción meteorológica, una herramienta de consultas
+geospaciales, notificación de incidencias y un modelo predictivo de ocupación. Además, el personal de gestión puede acceder a los 
+reportes de incidencias y al modelo predictivo de flujos.
 """)
 
 b1, b2, b3, b4 = st.columns([3,3,3,3])
