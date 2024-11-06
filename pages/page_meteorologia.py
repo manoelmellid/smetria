50504233e4a01589ce82ec758c0259455b0829d5
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-06 22:35:59 +0100

    Update page_meteorologia.py

--- pages/page_meteorologia.py
+++ pages/page_meteorologia.py
@@ -39,7 +39,7 @@ else:
 with st.form(key='my_form'):
     submit_button = st.form_submit_button(label='Enviar')
 
-# Verifica si el campo de texto no está vacío solo después de que se presiona el botón
+# Obtener las coordenadas del punto kilometrico
 if submit_button:
     longitud, latitud, concello_id, ubicacion = procesar_ubicacion(input_text, max_km_value, concam)
 
