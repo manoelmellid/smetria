06166c56c43c3215a524d2c36955bfdb95e50401
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-06 23:20:44 +0100

    Update ‎SMETRIA.py

--- ‎SMETRIA.py
+++ ‎SMETRIA.py
@@ -3,7 +3,7 @@ st.cache_data.clear()
 
 col1, col2, col3 = st.columns([3,3,3])
 with col1:
-    st.image("amtega_logo.png_2089811488.png", use_column_width=True)
+    st.image("amtega_logo.png_2089811488.png", use_container_width=True)
 with col3:
     st.header("SMETRIA")
 
