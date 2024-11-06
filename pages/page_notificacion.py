b99ffe33aa04858a5d0fdba63db90c3f385e8ce1
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-06 10:04:20 +0100

    Update page_notificacion.py

--- pages/page_notificacion.py
+++ pages/page_notificacion.py
@@ -9,21 +9,6 @@ def guardar_en_archivo(nombre, email, mensaje):
         writer = csv.writer(file)
         writer.writerow([nombre, email, mensaje])
 
-def visualizar_archivo():
-    # Leer el archivo contactos.csv y cargarlo en una lista
-    try:
-        with open('contactos.csv', mode='r') as file:
-            reader = csv.reader(file)
-            datos = list(reader)
-            if datos:
-                # Crear un dataframe a partir de los datos y mostrarlo
-                st.write("### Incidencias Registradas")
-                st.dataframe(datos)  # Puedes usar st.table(datos) si no quieres la funcionalidad interactiva
-            else:
-                st.write("No hay datos disponibles.")
-    except FileNotFoundError:
-        st.write("El archivo contactos.csv no existe aún.")
-
 # Campos del formulario
 nombre = st.text_input("Nombre")
 email = st.text_input("Correo electrónico")
