55a85c8c027e0977b0477b2e7e094286a2f4e8fc
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-07 11:41:48 +0100

    Update page_notificacion.py

--- pages/page_notificacion.py
+++ pages/page_notificacion.py
@@ -2,7 +2,8 @@ import streamlit as st
 import csv
 from utils import general as gen, github as git
 
-st.header("Sistema de notificación de incidencias - SINICAS")
+st.header("Sistema de notificación de incidencias")
+st.header("SINICAS")
 
 # Campos del formulario
 nombre = st.text_input("Nombre y apellidos")
