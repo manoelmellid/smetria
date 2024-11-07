8145757aed2648c9c865f01c95ad8f644d7f81fb
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-07 12:52:32 +0100

    Update page_visualizacion.py

--- pages/page_visualizacion.py
+++ pages/page_visualizacion.py
@@ -2,12 +2,14 @@ import streamlit as st
 import pandas as pd
 from utils import github as git
 
-# Cargar los datos especificando solo las columnas necesarias
+# Cargar los datos con solo las columnas necesarias
 df = git.cargar_datos(columnas_necesarias=['id', 'estado', 'fecha', 'latitud', 'longitud', 'tipo_incidencia'])
 
 # Filtrar el DataFrame para que solo contenga filas con 'estado' == 'Activo'
 df_activo = df[df['estado'] == 'Activo']
 
+# Renombrar las columnas para que Streamlit las reconozca como coordenadas
+df_activo = df_activo.rename(columns={'latitud': 'latitude', 'longitud': 'longitude'})
+
 # Mostrar el mapa con los puntos de las ubicaciones activas
-# Seleccionar solo las columnas 'latitud' y 'longitud' para el mapa
-st.map(df_activo[['latitud', 'longitud']])
+st.map(df_activo[['latitude', 'longitude']])
