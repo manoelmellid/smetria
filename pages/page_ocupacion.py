2027cbbcfee5484160dd081d9cb807463a310376
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-07 13:06:27 +0100

    Update page_ocupacion.py

--- pages/page_ocupacion.py
+++ pages/page_ocupacion.py
@@ -6,9 +6,3 @@ from utils import github as git
 st.header("Modelo predictivo de ocupacion")
 
 # ---------------------------------------------------------------------
-
-# Ejemplo de uso
-nombre = 'Juan Pérez'
-email = 'juan.perez@email.com'
-mensaje = 'Este es un mensaje de prueba.'
-git.guardar_en_archivo(nombre, email, mensaje)
