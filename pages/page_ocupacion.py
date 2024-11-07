a928aee824d29f0406f84fabc4446bc9ad7967c0
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-07 17:58:58 +0100

    Update page_ocupacion.py

--- pages/page_ocupacion.py
+++ pages/page_ocupacion.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.header("Modelo predictivo de ocupacion")
 
 # ---------------------------------------------------------------------
+
+tipo_seleccionado = st.multiselect("Selecciona el tipo de ubicación", ['Caca', 'Culo'], default=tipos[3])
