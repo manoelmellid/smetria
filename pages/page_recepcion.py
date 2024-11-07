f812df92431b24ecbb4a070e5d4e580781233fa9
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-07 13:19:55 +0100

    Update page_recepcion.py

--- pages/page_recepcion.py
+++ pages/page_recepcion.py
@@ -5,15 +5,17 @@ st.header("Sistema de recepción de incidencias")
 # ---------------------------------------------------------------------------------
 
 st.session_state.logged_in = gen.login()
+
 # Si está logueado, muestra las vistas según el rol
-if st.session_state.logged_in == True:
+if st.session_state.logged_in:
     st.write("Bienvenido al área de personal.")
     
-    # Cargar los datos filtrados
-    df = git.cargar_datos(columnas_necesarias=['id', 'estado', 'fecha', 'nombre', 'email', 'latitud', 'longitud', 'tipo_incidencia', 'comentario'])
+    # Cargar los datos solo si no están ya cargados en session_state
+    if 'df' not in st.session_state:
+        st.session_state.df = git.cargar_datos(columnas_necesarias=['id', 'estado', 'fecha', 'nombre', 'email', 'latitud', 'longitud', 'tipo_incidencia', 'comentario'])
 
     # Mostrar la tabla en Streamlit
-    st.dataframe(df)
+    st.dataframe(st.session_state.df)
     
     # Añadir un campo para ingresar un ID
     id_input = st.text_input("Ingrese el ID de la incidencia a solucionar:")
@@ -22,14 +24,14 @@ if st.session_state.logged_in == True:
     if st.button("Marcar como Solucionado"):
         # Verificar si el ID ingresado existe en el DataFrame
         if id_input:
-            if id_input in df['id'].values:  # Buscar el ID directamente en la columna 'id'
+            if id_input in st.session_state.df['id'].values:  # Buscar el ID directamente en la columna 'id'
                 # Actualizar el estado de la incidencia
-                df.loc[df['id'] == id_input, 'estado'] = 'Solucionado'
+                st.session_state.df.loc[st.session_state.df['id'] == id_input, 'estado'] = 'Solucionado'
                 st.success(f"La incidencia con ID {id_input} ha sido marcada como 'Solucionado'.")
             else:
                 st.error(f"No se encontró una incidencia con el ID {id_input}.")
         else:
             st.error("Por favor, ingrese un ID para la incidencia.")
 
-    # Mostrar la tabla actualizada
-    st.dataframe(df)
+    # Mostrar la tabla actualizada (sin recargar desde cero)
+    st.dataframe(st.session_state.df)
