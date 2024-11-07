15ad479f0d7db9fcd9e72e6c2e5b48d7e6a849d8
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-07 12:02:08 +0100

    Update page_recepcion.py

--- pages/page_recepcion.py
+++ pages/page_recepcion.py
@@ -8,4 +8,7 @@ st.session_state.logged_in = gen.login()
 # Si está logueado, muestra las vistas según el rol
 if st.session_state.logged_in == True:
     st.write("Bienvenido al área de personal.")
+    # Cargar los datos filtrados
+    df = git.cargar_datos(url_csv)
     # Mostrar la tabla en Streamlit
+    st.dataframe(df)
