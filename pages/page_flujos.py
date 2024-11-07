e84d61a2877332a6c84da4ed520de52563176976
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-07 17:22:29 +0100

    Update page_flujos.py

--- pages/page_flujos.py
+++ pages/page_flujos.py
@@ -3,4 +3,8 @@ from utils import general as gen
 
 st.header("Modelo predictivo de flujos")
 # ---------------------------------------------------------------------------------
-gen.login()
+
+st.session_state.logged_in = gen.login()
+# Si está logueado, muestra las vistas según el rol
+if st.session_state.logged_in == False:
+  st.wrtite("Bienvenido al área privada de Flujos - SMETRIA)
