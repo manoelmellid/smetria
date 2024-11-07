78124b0b12417b34695af8854189d298b34177e1
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-07 12:14:20 +0100

    Update page_recepcion.py

--- pages/page_recepcion.py
+++ pages/page_recepcion.py
@@ -9,6 +9,6 @@ st.session_state.logged_in = gen.login()
 if st.session_state.logged_in == True:
     st.write("Bienvenido al área de personal.")
     # Cargar los datos filtrados
-    df = git.cargar_datos(columnas_necesarias=['id', 'fecha', 'ubicacion', 'tipo_incidencia'])
+    df = git.cargar_datos(columnas_necesarias=['id', 'fecha', 'nombre', 'email', 'ubicacion', 'tipo_incidencia', 'comentario'])
     # Mostrar la tabla en Streamlit
     st.dataframe(df)
