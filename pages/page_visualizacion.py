253c7d6704fa068d38626a2f3e083694c295cea9
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-09 23:06:40 +0100

    Update page_visualizacion.py

--- pages/page_visualizacion.py
+++ pages/page_visualizacion.py
@@ -15,4 +15,37 @@ df_activo = df[df['estado'] == 'Activo']
 df_activo = df_activo.rename(columns={'latitud': 'latitude', 'longitud': 'longitude'})
 
 # Mostrar el mapa con los puntos de las ubicaciones activas
-st.map(df_activo[['latitude', 'longitude']])
+#st.map(df_activo[['latitude', 'longitude']])
+
+layer = pdk.Layer(
+    'ScatterplotLayer',
+    df_activo,
+    get_position='[longitude, latitude]',
+    get_radius=50000,  # Tamaño del punto
+    get_fill_color=[255, 0, 0, 160],  # Color del punto (rojo)
+    pickable=True,
+    auto_highlight=True
+)
+
+# Agregar etiquetas de texto (Tipo) sobre los puntos
+tooltip = {
+    "html": "<b>Tipo:</b> {Tipo}",  # Mostrar el tipo de cada ubicación
+    "style": {"color": "white", "backgroundColor": "rgba(0,0,0,0.7)", "padding": "5px"}
+}
+
+# Crear la vista del mapa
+view_state = pdk.ViewState(
+    latitude=df_activo['latitude'].mean(),
+    longitude=df_activo['longitude'].mean(),
+    zoom=5
+)
+
+# Renderizar el mapa con pydeck
+deck = pdk.Deck(
+    layers=[layer],
+    initial_view_state=view_state,
+    tooltip=tooltip
+)
+
+# Mostrar el mapa en Streamlit
+st.pydeck_chart(deck)
