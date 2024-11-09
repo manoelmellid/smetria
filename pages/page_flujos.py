317e92a22c7b3acd2eebacfaa2e3833a0cf9389b
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-09 17:56:07 +0100

    Update page_flujos.py

--- pages/page_flujos.py
+++ pages/page_flujos.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from utils import general as gen
+from utils import general as gen, rutas as rut
 
 st.header("Modelo predictivo de flujos")
 # ---------------------------------------------------------------------------------
@@ -8,3 +8,13 @@ st.session_state.logged_in = gen.login()
 # Si está logueado, muestra las vistas según el rol
 if st.session_state.logged_in == False:
   st.success("Bienvenido al área privada de Flujos - SMETRIA")
+
+
+st.title("Ruta a Pie entre dos Puntos")
+
+# Definir las coordenadas de origen y destino
+origen = [-33.4489, -70.6693]  # Santiago, Chile (Lat, Long)
+destino = [-33.4637, -70.6483]  # Barrio Lastarria, Santiago, Chile (Lat, Long)
+
+# Mostrar el mapa con la ruta
+rut.mostrar_mapa(origen, destino)
