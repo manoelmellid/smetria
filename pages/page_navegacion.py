098436ac5510595b7c3ce4bc8fa38ba52e864510
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-09 18:38:16 +0100

    Update page_navegacion.py

--- pages/page_navegacion.py
+++ pages/page_navegacion.py
@@ -122,19 +122,19 @@ if submit_button:
         ))
 
         # Create an HTML column for color sample
-    df_filtrado['color_muestra_html'] = df_filtrado['color'].apply(
-        lambda color: f'<div style="background-color: rgb({color[0]}, {color[1]}, {color[2]}); width: 30px; height: 30px;"></div>'
-    )
-
-    # Display the DataFrame with color samples using st.markdown
-    st.markdown("<h3>Resultados de Ubicaciones</h3>", unsafe_allow_html=True)
-    for _, row in df_filtrado[['enderezo', 'nome', 'distancia_km', 'lon', 'lat', 'color_muestra_html']].iterrows():
-        st.markdown(f"""
-            <div style="display: flex; align-items: center;">
-                <div style="width: 100px;">{row['nome']}</div>
-                <div style="width: 100px;">{row['distancia_km']:.2f} km</div>
-                <div>{row['color_muestra_html']}</div>
-            </div>
-            """, unsafe_allow_html=True)
+        df_filtrado['color_muestra_html'] = df_filtrado['color'].apply(
+            lambda color: f'<div style="background-color: rgb({color[0]}, {color[1]}, {color[2]}); width: 30px; height: 30px;"></div>'
+        )
+    
+        # Display the DataFrame with color samples using st.markdown
+        st.markdown("<h3>Resultados de Ubicaciones</h3>", unsafe_allow_html=True)
+        for _, row in df_filtrado[['enderezo', 'nome', 'distancia_km', 'lon', 'lat', 'color_muestra_html']].iterrows():
+            st.markdown(f"""
+                <div style="display: flex; align-items: center;">
+                    <div style="width: 100px;">{row['nome']}</div>
+                    <div style="width: 100px;">{row['distancia_km']:.2f} km</div>
+                    <div>{row['color_muestra_html']}</div>
+                </div>
+                """, unsafe_allow_html=True)
 else:
     st.warning("Por favor, introduce una distancia en kilómetros.")
