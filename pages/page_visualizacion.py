95cd4bea4497ece09132de3ab87ab39e0eedfb53
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-09 23:14:38 +0100

    Update page_visualizacion.py

--- pages/page_visualizacion.py
+++ pages/page_visualizacion.py
@@ -4,20 +4,24 @@ import pydeck as pdk
 from utils import github as git
 
 st.header("Mapa de visualización de incidencias")
-# ---------------------------------------------------------------------------------
 
+# ---------------------------------------------------------------------------------
 # Cargar los datos con solo las columnas necesarias
 df = git.cargar_datos(columnas_necesarias=['id', 'estado', 'fecha', 'latitud', 'longitud', 'tipo_incidencia'])
 
 # Filtrar el DataFrame para que solo contenga filas con 'estado' == 'Activo'
 df_activo = df[df['estado'] == 'Activo']
 
-# Renombrar las columnas para que Streamlit las reconozca como coordenadas
-df_activo = df_activo.rename(columns={'latitud': 'latitude', 'longitud': 'longitude'})
+# Asegurarse de que las coordenadas sean de tipo float
+df_activo['latitude'] = pd.to_numeric(df_activo['latitud'], errors='coerce')
+df_activo['longitude'] = pd.to_numeric(df_activo['longitud'], errors='coerce')
 
-# Mostrar el mapa con los puntos de las ubicaciones activas
 #st.map(df_activo[['latitude', 'longitude']])
 
+# Renombrar las columnas para que Streamlit las reconozca como coordenadas
+# Ya lo hicimos con pd.to_numeric, no necesitamos renombrar de nuevo
+
+# Crea la capa para el mapa
 layer = pdk.Layer(
     'ScatterplotLayer',
     df_activo,
@@ -28,9 +32,9 @@ layer = pdk.Layer(
     auto_highlight=True
 )
 
-# Agregar etiquetas de texto (Tipo) sobre los puntos
+# Agregar etiquetas de texto (Tipo de incidencia) sobre los puntos
 tooltip = {
-    "html": "<b>Tipo:</b> {Tipo}",  # Mostrar el tipo de cada ubicación
+    "html": "<b>Tipo:</b> {tipo_incidencia}",  # Mostrar el tipo de cada ubicación
     "style": {"color": "white", "backgroundColor": "rgba(0,0,0,0.7)", "padding": "5px"}
 }
 
@@ -38,7 +42,9 @@ tooltip = {
 view_state = pdk.ViewState(
     latitude=df_activo['latitude'].mean(),
     longitude=df_activo['longitude'].mean(),
-    zoom=12
+    zoom=12,  # Puedes ajustar el zoom según el alcance de las coordenadas
+    pitch=0,  # Sin inclinación
+    bearing=0  # Sin rotación
 )
 
 # Renderizar el mapa con pydeck
