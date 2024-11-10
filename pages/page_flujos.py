60e3911d6ea0641d18b102577e791caa9ac76215
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-10 21:29:51 +0100

    Update page_flujos.py

--- pages/page_flujos.py
+++ pages/page_flujos.py
@@ -8,4 +8,5 @@ st.session_state.logged_in = gen.login()
 # Si está logueado, muestra las vistas según el rol
 if st.session_state.logged_in == False:
   st.success("Bienvenido al área privada de Flujos - SMETRIA")
+  input_text = st.text_input("Indica el Km del Camino dónde te encuentras")
 
