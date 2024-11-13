3b2e814c58ca18a422fdd239d3d65dab560b5e65
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-13 10:59:45 +0100

    Update page_recepcion.py

--- pages/page_recepcion.py
+++ pages/page_recepcion.py
@@ -12,20 +12,11 @@ if st.session_state.logged_in == False:
     df = git.cargar_datos(columnas_necesarias=['id', 'estado', 'fecha', 'latitud', 'longitud', 'tipo_incidencia', 'tipo_alerta', 'comentario'])
     # Mostrar la tabla en Streamlit
     st.write(df)
-    id_input = st.text_input("Ingrese el ID de la incidencia a solucionar:")
-    # Botón para cambiar el estado
-    if st.button("Marcar como Solucionado"):
-        if id_input:
-            if id_input in df['id'].values:
-                git.actualizar_estado(id_input)
-            else:
-                st.error(f"No se encontró una incidencia con el ID {id_input}.")
-        else:
-            st.error("Por favor, ingrese un ID para la incidencia.")
-
+    
 tipo_opc = st.selectbox('Selecciona la opción:', ['Cambio de estado', 'Cambio del nivel de alerta', 'Cambio de tipo'])
 
 if tipo_opc == 'Cambio de estado':
+    id_input = st.text_input("Ingrese el ID de la incidencia a solucionar:")
     if st.button("Marcar como Solucionado"):
         if id_input:
             if id_input in df['id'].values:
