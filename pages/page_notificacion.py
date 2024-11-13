4149ac633a36570362928908d1271f166e8b688d
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-13 10:17:10 +0100

    Update page_notificacion.py

--- pages/page_notificacion.py
+++ pages/page_notificacion.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import csv
+import pandas as pd
 from utils import general as gen, github as git
 
 st.header("Sistema de notificación de incidencias")
+
+opciones_df = pd.read_csv('opciones_incidencias.csv')
 
 # Campos del formulario
 nombre = st.text_input("Nombre y apellidos")
@@ -15,7 +18,8 @@ if telefono:
 email = st.text_input("Correo electrónico")
 input_text = st.text_input("Indique el Km del Camino dónde se encuentra")
 opciones = ['Crecida de río', 'Incendio', 'Desprendimiento de tierra', 'Tramo colapsado/cerrado', 'Mobiliario deteriorado', 'Accidente en el camino', 'Animales sueltos', 'Fuente sin agua']
-tipo_opc = st.selectbox('Selecciona el tipo de incidencia:', opciones)
+tipo_opc = st.selectbox('Selecciona el tipo de incidencia:', opciones_df['tipo'].tolist())
+alerta_opc = opciones_df.loc[opciones_df['tipo'] == tipo_opc, 'alerta'].iloc[0]
 mensaje = st.text_area("Añada más detalles si lo considera necesario:")
 
 if st.button("Enviar"):
@@ -28,6 +32,7 @@ if st.button("Enviar"):
             input_text=input_text,
             tipo_opc=tipo_opc,
             mensaje=mensaje,
+            alerta_opc=alerta_opc
         )
         st.success("Notificación enviada con éxito.")
     else:
