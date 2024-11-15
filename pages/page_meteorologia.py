c7abfdf7efcfee480f90d65bf1d694ddac68c56a
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-15 19:49:10 +0100

    Update page_meteorologia.py

--- pages/page_meteorologia.py
+++ pages/page_meteorologia.py
@@ -41,7 +41,7 @@ with st.form(key='my_form'):
 
 # Obtener las coordenadas del punto kilometrico
 if submit_button:
-    longitud, latitud, concello_id, ubicacion = concam.procesar_ubicacion(input_text, "vertices_250_camino_pt.csv")
+    longitud, latitud, concello_id, ubicacion = concam.procesar_ubicacion(input_text)
     adelante=1
 
 if concello_id is not None:
