a5e1468ef99a67761a9068001b54492d45abc5f7
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-16 13:57:11 +0100

    Update 2_page_navegacion.py

--- pages/2_page_navegacion.py
+++ pages/2_page_navegacion.py
@@ -35,9 +35,8 @@ elif camino == "Camino del Norte":
 # Cargar el archivo CSV
 def cargar_datos():
     return pd.read_csv("puntos_interes.csv")
+    
 df = cargar_datos()
-
-# Convertir datos en un GeoDataFrame
 df['geometry'] = df['geom'].apply(lambda x: Point(map(float, x.replace("POINT (", "").replace(")", "").split())))
 gdf = gpd.GeoDataFrame(df, geometry='geometry')
 gdf['lat'] = gdf['geometry'].y
@@ -47,10 +46,8 @@ gdf['lon'] = gdf['geometry'].x
 tipos = df['tipo'].unique()
 tipo_seleccionado = st.multiselect("Selecciona el tipo de ubicación", tipos, default=tipos[3] if len(tipos) > 3 else [])
 
-# Campo de entrada para el km del Camino
+# Parámetros de búsqueda
 input_text = st.text_input("Indica el Km del Camino dónde te encuentras")
-
-# Barra deslizante para seleccionar el radio de distancia
 radio_km = st.slider("Radio de distancia (km)", min_value=1, max_value=10, value=5)
 
 # Crear un formulario para procesar la búsqueda al enviar
@@ -63,27 +60,27 @@ if 'longitud' in st.session_state and 'latitud' in st.session_state:
     latitud = st.session_state.latitud
 
 if submit_button:
-    # Procesar la ubicación solo si el botón es presionado
-    longitud, latitud, concello_id, ubicacion, km = concam.procesar_ubicacion(input_text, archivo)
-
-    # Guardar en session_state para mantener el estado entre interacciones
-    st.session_state.longitud = longitud
-    st.session_state.latitud = latitud
-
-    if longitud is None and latitud is None:
-        st.error("No se encontraron resultados para el valor de Km proporcionado.")
-    else:
-        # Filtrar el dataframe por tipo de ubicación seleccionado
-        df_filtrado = gdf[gdf['tipo'].isin(tipo_seleccionado)]
-
-        # Calcular distancias
+    longitud, latitud, _, _, _ = concam.procesar_ubicacion(input_text, archivo)
+    if longitud and latitud:
+        st.session_state.longitud = longitud
+        st.session_state.latitud = latitud
         punto_usuario = (latitud, longitud)
+
+        # Filtrar el dataframe por tipo y calcular distancias
+        df_filtrado = gdf[gdf['tipo'].isin(tipo_seleccionado)]
         df_filtrado['distancia_km'] = df_filtrado['geometry'].apply(
             lambda x: geodesic(punto_usuario, (x.y, x.x)).km
         )
         df_filtrado = df_filtrado[df_filtrado['distancia_km'] <= radio_km]
-        st.write(df_filtrado)
-        
+        st.session_state['df_filtrado'] = df_filtrado
+        st.write(st.session_state['df_filtrado'])
+
+    else:
+        st.error("No se encontraron resultados para el valor de Km proporcionado.")
+
+    if st.session_state['df_filtrado'] is not None;
+        st.write(st.session_state['df_filtrado'])
+    
         m = folium.Map(location=[latitud, longitud], zoom_start=12)
         marker_cluster = MarkerCluster().add_to(m)
 
