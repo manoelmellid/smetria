ebac92c67444592b23c4b24bd9569d7d3b09b90d
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-11-16 13:42:21 +0100

    Update 2_page_navegacion.py

--- pages/2_page_navegacion.py
+++ pages/2_page_navegacion.py
@@ -166,9 +166,5 @@ if submit_button:
 
 else:
     st.warning("Por favor, introduce una distancia en kilómetros.")
-
-# Recuperar df_filtrado de session_state, si está disponible
-df_filtrado = st.session_state.get('df_filtrado', None)
-
-if df_filtrado is not None:
-    rut.mostrar_seleccion(df_filtrado, latitud, longitud)
+st.write(df_filtrado)
+rut.mostrar_seleccion(df_filtrado, latitud, longitud)
