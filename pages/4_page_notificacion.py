94d67edc8b4fd2c1413f4f155d62d4b32d4863c6
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-12-04 23:08:45 +0100

    Update 4_page_notificacion.py

--- pages/4_page_notificacion.py
+++ pages/4_page_notificacion.py
@@ -29,13 +29,12 @@ telefono = st.text_input("Teléfono")
 if telefono:
     if not gen.validar_telefono(telefono):
         st.error("Por favor, ingrese un número de teléfono válido.")
-        
-email = st.text_input("Correo electrónico")
-camino, archivo, abrv = gen.camino()
-input_text = st.text_input("Indique el Km del Camino dónde se encuentra")
-
-# Campo para el mensaje de incidencia
-comentario = st.text_area("Añada una descripción:")
+    else:
+        email = st.text_input("Correo electrónico")
+    camino, archivo, abrv = gen.camino()
+    input_text = st.text_input("Indique el Km del Camino dónde se encuentra")
+    # Campo para el mensaje de incidencia
+    comentario = st.text_area("Añada una descripción:")
 
 # Preprocesamiento y clasificación del tipo de incidencia
 if comentario:
