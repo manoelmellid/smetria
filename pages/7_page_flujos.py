e51ede4aa9663eff1de90e2add40f4ab08df868a
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-12-04 19:56:46 +0100

    Update 7_page_flujos.py

--- pages/7_page_flujos.py
+++ pages/7_page_flujos.py
@@ -11,39 +11,3 @@ if st.session_state.logged_in == False:
   #input_text = st.text_input("Indica el Km del Camino dónde te encuentras")
   camino = gen.camino()
 # ---------------------------------------------------------------------------------
-
-import streamlit as st
-import joblib
-import spacy
-
-# Cargar modelo de spaCy para preprocesamiento
-nlp = spacy.load("es_core_news_sm")
-
-# Función para preprocesar texto
-def preprocesar_texto(texto):
-    doc = nlp(texto)
-    return ' '.join([token.lemma_ for token in doc if not token.is_stop])
-
-# Cargar el modelo entrenado y el vectorizador
-modelo_svm = joblib.load('modelo_svm.sav')
-vectorizador = joblib.load('vectorizador_tfidf.sav')
-
-# Título de la aplicación
-st.title("Clasificador de Incidencias")
-
-# Campo de entrada para el comentario
-comentario = st.text_input("Escribe el comentario aquí:")
-
-if comentario:
-    # Preprocesar el comentario ingresado
-    comentario_preprocesado = preprocesar_texto(comentario)
-    
-    # Vectorizar el comentario usando el vectorizador cargado
-    comentario_vectorizado = vectorizador.transform([comentario_preprocesado])
-    
-    # Hacer la predicción
-    prediccion = modelo_svm.predict(comentario_vectorizado)
-    
-    # Mostrar la predicción
-    st.write(f"El tipo de incidencia clasificada es: **{prediccion[0]}**")
-
