a6f60a6324aac358ed3bbd5e1a8b4b8b0eebcdc4
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-12-12 18:34:03 +0100

    Update ‎SMETRIA.py

--- ‎SMETRIA.py
+++ ‎SMETRIA.py
@@ -8,7 +8,7 @@ with col3:
     st.header("SMETRIA")
 
 st.markdown("<h3 style='text-align: center;'>Sistema de monitorización de eventos en tramos</h3>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center;'>Camino de Santiago - Camino Portugués</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;'>Camino de Santiago</h3>", unsafe_allow_html=True)
 st.divider()
 
 pages = {
