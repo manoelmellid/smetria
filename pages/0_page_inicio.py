4cb9bedac8573b382e9609dd41d5b4ca9cf069b2
manoelmellid <153660396+manoelmellid@users.noreply.github.com>
2024-12-13 18:17:01 +0000

    Update 0_page_inicio.py

--- pages/0_page_inicio.py
+++ pages/0_page_inicio.py
@@ -18,8 +18,9 @@ with b6:
     if st.button("Mapa de incidencias"):
         st.switch_page("pages/5_page_mapaincidencias.py")
 
-st.write("\nPara el personal que gestiona los Caminos de Santiago, hemos desarrollado dos herramientas que agilizan su trabajo")
-st.write("Dentro de un área privada pueden consultar el modelo predictivo de flujos y revisar los reportes de incidencias, con la capacidad de activar y desactivar las incidencias reportadas según sea necesario.")
+st.write("""\nPara el personal que gestiona los Caminos de Santiago, hemos desarrollado dos herramientas que agilizan su trabajo.
+Dentro de un área privada pueden consultar el modelo predictivo de flujos y revisar los reportes de incidencias, con la capacidad 
+de activar y desactivar las incidencias reportadas según sea necesario.""")
 c1, c2 = st.columns([2,2])
 with c1:
     if st.button("Recepción de incidencias"):
